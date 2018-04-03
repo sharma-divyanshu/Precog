c02a9f901740c5d8028e84f3b3ec2694f0001eb0
SerDjent <divyanshu8sharma@gmail.com>
2018-04-04 02:59:51 +0530

    GCP friendly Directory

--- repo.py
+++ repo.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from keras.preprocessing import image
 from keras.models import load_model
+import h5py
 
 def detect_faces(location, filename, app_dir):
     face_cascade = cv2.CascadeClassifier(os.path.join(app_dir, 'haarcascades/haarcascade_frontalface_default.xml'))
@@ -12,14 +13,15 @@ def detect_faces(location, filename, app_dir):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(process_img, (x,y), (x+w, y+h), (255,255,0), 1)
-    processed_images = os.path.join(app_dir, "static/etc/")
+    processed_images = os.path.join(app_dir, 'static/images/')
     if not os.path.isdir(processed_images):
         os.mkdir(processed_images)
     cv2.imwrite(processed_images + filename, process_img)
-    return len(faces), processed_images + filename
+    return len(faces), 'static/images/' + str(filename)
 
 def isModi(location, filename, app_dir):
     classifier = load_model(os.path.join(app_dir, 'classifiers/modi.h5'))
+    classifier.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     test_image = image.load_img(location, target_size = (150,150))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis = 0)
@@ -31,6 +33,7 @@ def isModi(location, filename, app_dir):
 
 def isKejriwal(location, filename, app_dir):
     classifier = load_model(os.path.join(app_dir, 'classifiers/kejriwal.h5'))
+    classifier.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     test_image = image.load_img(location, target_size = (150,150))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis = 0)
